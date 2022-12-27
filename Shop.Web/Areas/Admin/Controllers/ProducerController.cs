bed9b3b1e33ba0620661f3960f358203af3fb5bb
duonnh200 <duongnh@sphinxjsc.com>
2022-12-27 17:44:05 +0700

    feat: create view Producer

---
+++ Shop.Web/Areas/Admin/Controllers/ProducerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.Web.Areas.Admin.Controllers
+{
+    public class ProducerController : Controller
+    {
+        // GET: Admin/Producer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}

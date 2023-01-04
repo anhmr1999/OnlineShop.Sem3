1c5ea7ac692a981a1d81e116fa6777acda1978d2
anhnh <Welcome@DESKTOP-HNG1KT1>
2023-01-04 16:35:52 +0700

    CRU role done

--- Shop.Web/Areas/Admin/Controllers/RoleController.cs
+++ Shop.Web/Areas/Admin/Controllers/RoleController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Shop.EntityFramework.Entities;
+using Shop.EntityFramework.Infrastructures.Permissions;
+using Shop.EntityFramework.Infrastructures.Repository;
+using Shop.Web.Common;
+using Shop.Web.Common.ObjectRequests;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,19 +13,101 @@ namespace Shop.Web.Areas.Admin.Controllers
 {
     public class RoleController : Controller
     {
-        // GET: Admin/Role
-        public ActionResult Index()
+        private readonly IRepository<Role> _roleRepository;
+        private readonly IRepository<PermissionGrant> _permissionGrantRepository;
+
+        public RoleController(IRepository<Role> roleRepository, IRepository<PermissionGrant> permissionGrantRepository)
         {
-            return View();
+            _roleRepository = roleRepository;
+            _permissionGrantRepository = permissionGrantRepository;
         }
 
-        public ActionResult Edit()
+        // GET: Admin/Role
+        public ActionResult Index(CommonFilter filter)
         {
-            return View();
+            var query = _roleRepository.GetQueryable()
+                .WhereIf(!string.IsNullOrEmpty(filter.SearchKey), x => x.RoleName.ToLower().Contains(filter.SearchKey.ToLower()));
+
+            var model = new CommonListResult<Role>()
+            {
+                Filter = filter,
+                List = query.OrderByDescending(x => x.CreationTime).Skip((filter.PageIndex - 1) * 10).Take(10).ToList(),
+                TotalCount = query.Count(),
+                TotalPage = (int)Math.Ceiling((decimal)query.Count()/10)
+            };
+            return View(model);
         }
+
         public ActionResult Add()
         {
-            return View();
+            return View(new RoleObject());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(RoleObject roleDto)
+        {
+            if(!ModelState.IsValid)
+                return View(roleDto);
+
+            if(_roleRepository.Any(x => x.RoleName == roleDto.RoleName))
+            {
+                ModelState.AddModelError(nameof(RoleObject.RoleName), "Role name is exists");
+                return View(roleDto);
+            }
+
+            var role = new Role();
+            role.RoleName= roleDto.RoleName;
+            role.IsDefault= roleDto.IsDefault;
+            role.IsStatic = roleDto.IsStatic;
+            _roleRepository.Insert(role);
+            _permissionGrantRepository.InsertMany(roleDto.Permissions.Select(x => new PermissionGrant() { Name = x, ProviderKey = role.RoleName, ProviderName = "R"}));
+            _roleRepository.SaveChange();
+            _permissionGrantRepository.SaveChange();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(Guid id)
+        {
+            var role = _roleRepository.Get(id);
+            if (role == null)
+                return RedirectToAction("Index");
+
+            var dto = new RoleObject();
+            dto.RoleName = role.RoleName;
+            dto.IsDefault = role.IsDefault;
+            dto.IsStatic = role.IsStatic;
+            var permissions = _permissionGrantRepository.GetList(x => x.ProviderName == "R" && x.ProviderKey == role.RoleName);
+            dto.Permissions = permissions.Select(x => x.Name).ToArray();
+            return View(dto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Guid id, RoleObject roleDto)
+        {
+            if (!ModelState.IsValid)
+                return View(roleDto);
+
+            var role = _roleRepository.Get(id);
+
+            if(_roleRepository.Any(x => x.Id != id && x.RoleName == roleDto.RoleName))
+            {
+                ModelState.AddModelError(nameof(RoleObject.RoleName), "Role name is exists");
+                return View(roleDto);
+            }   
+            
+            role.RoleName = roleDto.RoleName;
+            role.IsDefault = roleDto.IsDefault;
+            role.IsStatic = roleDto.IsStatic;
+            _roleRepository.Update(role);
+
+            var permissions = _permissionGrantRepository.GetList(x => x.ProviderName == "R" && x.ProviderKey == role.RoleName);
+            _permissionGrantRepository.DeleteMany(permissions);
+            _permissionGrantRepository.InsertMany(roleDto.Permissions.Select(x => new PermissionGrant() { Name = x, ProviderKey = role.RoleName, ProviderName = "R" }));
+            _roleRepository.SaveChange();
+            _permissionGrantRepository.SaveChange();
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Shop.Web/Common/ObjectRequests/RoleObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Shop.Web.Common.ObjectRequests
+{
+    public class RoleObject
+    {
+        [Required(ErrorMessage = "Role name is not empty!")]
+        public string RoleName { get; set; }
+        public bool IsDefault { get; set; }
+        public bool IsStatic { get; set; }
+        public string[] Permissions { get; set; } = new string[0];
+    }
+}

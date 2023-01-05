0951b8e4e3c9b6fb215121df1402da3ffefba4be
duonnh200 <duongnh@sphinxjsc.com>
2023-01-05 14:12:26 +0700

    feat: CRUD News

--- Shop.Web/Areas/Admin/Controllers/NewsController.cs
+++ Shop.Web/Areas/Admin/Controllers/NewsController.cs
@@ -1,5 +1,6 @@
 ﻿using Shop.EntityFramework.Entities;
 using Shop.EntityFramework.Infrastructures.Repository;
+using Shop.Web.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,31 +18,79 @@ namespace Shop.Web.Areas.Admin.Controllers
             _newsRepository = newsRepository;
         }
         // GET: Admin/News
-        public ActionResult Index()
+        public ActionResult Index(CommonFilter filter)
+        {
+            var query = _newsRepository.GetQueryable()
+              .WhereIf(!string.IsNullOrEmpty(filter.SearchKey), x => x.Name.ToLower().Contains(filter.SearchKey) || x.Description.ToLower().Contains(filter.SearchKey));
+            var model = new CommonListResult<News>();
+            model.Filter = filter;
+            model.TotalCount = query.Count();
+            model.TotalPage = Math.Ceiling((decimal)query.Count() / 10);
+            model.List = query.OrderByDescending(x => x.CreationTime).PagedBy(filter).ToList();
+            return View(model);
+        }
+
+        // GET: Admin/Category/Add
+        public ActionResult Add()
         {
             return View(new News());
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Add(News news)
+        public ActionResult Add(News newsDto)
         {
             if (!ModelState.IsValid)
-                return View(news);
-
-            if (_newsRepository.Any(x => x.Code.ToLower() == news.Code.ToLower()))
+                return View(newsDto);
+            if (_newsRepository.Any(x => x.Code.ToLower() == newsDto.Code.ToLower()))
             {
-                ModelState.AddModelError(nameof(News.Code), $"code {news.Code} has been used");
-                return View(news);
+                ModelState.AddModelError(nameof(News.Code), $"code {newsDto.Code} has been used");
+                return View(newsDto);
             }
-
-            news.Code = news.Code.ToLower();
+            var news =new News();
+            news.Code = newsDto.Code.ToLower();
+            news.Name = newsDto.Name;
+            news.Description = newsDto.Description;
+            news.ShortDesciption = newsDto.ShortDesciption;
+            news.Image = newsDto.Image;
+            news.IsGlobal = newsDto.IsGlobal;
             _newsRepository.Insert(news);
             _newsRepository.SaveChange();
             return RedirectToAction("Index");
-        }
-        public ActionResult Edit()
+        } 
+        // POST: Admin/Edit
+        public ActionResult Edit(Guid id)
         {
-            return View();
+            var sale = _newsRepository.Get(id);
+            if (sale == null)
+                return RedirectToAction("Index");
+            return View(sale);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Guid id, News newsDto)
+        {
+            if (!ModelState.IsValid)
+                return View(newsDto);
+            var news = _newsRepository.Get(id);
+            news.Code = newsDto.Code.ToLower();
+            news.Name = newsDto.Name;
+            news.Description = newsDto.Description;
+            news.ShortDesciption = newsDto.ShortDesciption;
+            news.Image = newsDto.Image;
+            news.IsGlobal = newsDto.IsGlobal;
+            _newsRepository.Update(news);
+            _newsRepository.SaveChange();
+            return RedirectToAction("Index");
+        }
+        //DELETE: 
+        public ActionResult Delete(Guid id)
+        {
+            var sale = _newsRepository.Get(id);
+            if (sale == null)
+                return RedirectToAction("Index");
+            _newsRepository.Delete(sale);
+            _newsRepository.SaveChange();
+            return RedirectToAction("Index");
         }
     }
 }
